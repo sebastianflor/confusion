2541a9ae56d2cf946e501110ee9d031c232a5a03
sebastianflor <sebastian-flor@hotmail.com>
2019-08-04 16:43:19 +0200

    Components part II

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 
 import Menu from './components/menu';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='#'>Jsebasf Restaurant</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
-}
+import { DISHES } from './shared/dishes';
 
-export default App;
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='#'>JSEBASF Restaurant</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
