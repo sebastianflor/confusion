96754e909353d4f05ad4929ca6169252664249a0
sebastianflor <sebastian-flor@hotmail.com>
2019-08-09 12:17:35 +0200

    Presentational and Container Components II

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,27 +2,20 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Badge } from 'reactstrap';
 
 export default class DishDetail extends React.Component {
-    renderDetail = (dish) => {
-        if(dish) {
+    renderDetail = (dish) => {       
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                        <CardImg top src={dish.image} alt={dish.name} />
                         <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
                 </div>
             );
-        } else {
-            return (
-                <div></div>
-            );
-        }
     }
     renderComment = (dish) => {
-        if(dish) {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <h4>Comments</h4>
@@ -38,20 +31,21 @@ export default class DishDetail extends React.Component {
                     </ul>
                 </div>
             );
+    }
+    render() {       
+        if(this.props.dish != null){
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        {this.renderDetail(this.props.dish)}
+                        {this.renderComment(this.props.dish)}
+                    </div>
+                </div>
+            );
         } else {
             return (
                 <div></div>
             );
         }
-    }
-    render() {       
-        return (
-            <div className='container'>
-                <div className='row'>
-                    {this.renderDetail(this.props.dish)}
-                    {this.renderComment(this.props.dish)}
-                </div>
-            </div>
-        );
     }
 }
